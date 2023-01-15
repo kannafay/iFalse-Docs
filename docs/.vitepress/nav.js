71875e871d1fb4c0c85f1f83c7980d184ad71ae6
神秘布偶猫 <kannafay@qq.com>
2023-01-15 23:12:36 +0800

    23.1.15

--- docs/.vitepress/nav.js
+++ docs/.vitepress/nav.js
@@ -3,4 +3,5 @@ module.exports = [
   { text: '说明', link: '/explain/' },
   { text: '主题配置', link: '/options/' },
   { text: '小工具配置', link: '/widgets/' },
+  { text: '主题演示', link: 'https://ifalse.onll.cn' },
 ]
