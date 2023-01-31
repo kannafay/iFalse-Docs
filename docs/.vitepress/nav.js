fd886f03abbf195ff6275311de4492456e21b0a6
神秘布偶猫 <kannafay@qq.com>
2023-02-01 01:35:52 +0800

    Update nav.js

--- docs/.vitepress/nav.js
+++ docs/.vitepress/nav.js
@@ -2,5 +2,5 @@ module.exports = [
   { text: '主页', link: '/' },
   { text: '常见问题', link: '/explain/' },
   { text: '开始配置', link: '/options/' },
-  { text: '主题演示', link: 'https://ifalse.onll.cn' },
+  { text: '主题官网', link: 'https://ifalse.onll.cn' },
 ]
